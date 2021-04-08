35b28e6c6e8fe12200093a61ff65b171f1afa2af
Tom Goldstein <tomgoldstein@ip-192-168-1-48.us-west-2.compute.internal>
2021-04-08 00:37:57 -0400

    minor

--- create_raw_audio_dataset.py
+++ create_raw_audio_dataset.py
@@ -88,13 +88,15 @@ flight_names = set(i[0] for i in gt_valid.index)
 
 for n in flight_names:  # Loop over flight names
     print('    Processing ', n)
-    try:
-        time_arr, audio_arr, gt_interp = create_interpolated_ground_truth(n)
-        grp = f.create_group(n)
-        grp['timestamp'] = time_arr
-        grp['audio'] = audio_arr
-        grp['ground_truth'] = gt_interp
-    except Exception as e:
-        print(str(e))
+    if n not in f.keys():
+        try:
+            time_arr, audio_arr, gt_interp = create_interpolated_ground_truth(n)
+            grp = f.create_group(n)
+            grp['timestamp'] = time_arr
+            grp['audio'] = audio_arr
+            grp['ground_truth'] = gt_interp
+            f.flush()
+        except Exception as e:
+            print(str(e))
 
 f.close()
