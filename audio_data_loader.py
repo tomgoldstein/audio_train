1fbafb2b3e421a51f6221916e495eb3ee7388e4c
Tom Goldstein <tomgoldstein@ip-192-168-1-48.us-west-2.compute.internal>
2021-04-13 00:44:46 -0400

    minor

--- audio_data_loader.py
+++ audio_data_loader.py
@@ -1,10 +1,16 @@
-import torch.utils.data.Dataset as Dataset
+import torch
+from torch.utils.data import Dataset
+import h5py
+import numpy as np
+
 
 class AudioDataset(Dataset):
-    """h5_file is an hdf5 file (readable by h5py) that contains many groups.  Within each
+    """h5_file: an hdf5 file (readable by h5py) that contains many groups.  Within each
     group is a timestamp (Nx1 float array), a gt_dist (Nx1 float array of ground truth distances in meters),
-    and an array of audio samples from the microphone array (Nx8 float array)"""
-    def __init__(self, h5_file, root_dir, transform=None):
+    and an array of audio samples from the microphone array (Nx8 float array)
+    length_of_sample: the number of data points to include in each sample.  A sample will be a contiguous chunk
+    of the arrays stored in the h5 file.  The length of this chunk is length_of_sample."""
+    def __init__(self, h5_file, length_of_sample):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -12,26 +18,40 @@ class AudioDataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        self.landmarks_frame = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
+        self.h5_file = h5_file
+        self.keys = list(h5_file.keys())
+        self.length_of_sample = length_of_sample
+
 
     def __len__(self):
-        return len(self.landmarks_frame)
+       return len(self.keys)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        data = self.h5_file[self.keys[idx]]
+        timestamp = data['timestamp']
+        audio     = data['audio']
+        dist      = data['dist']
+        angle      = data['angle']
+        los = self.length_of_sample
 
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array([landmarks])
-        landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
+        start = np.random.randint(0, len(timestamp)-los )
+        return timestamp[start:start+los], audio[start:start+los], dist[start:start+los], angle[start:start+los]
 
-        if self.transform:
-            sample = self.transform(sample)
 
-        return sample
+# f = h5py.File('temp.h5','w')
+# grp = f.create_group('test_group')
+# a = np.linspace(0,100,101)
+# grp['timestamp'] = a
+# grp['audio'] = a+0.1
+# grp['dist'] = a+0.2
+# grp['angle'] = a+0.3
+# f.close()
+
+f = h5py.File('temp.h5','r')
+ds = AudioDataset(f, length_of_sample=10)
+
+ts, audio, dist, angle = ds[0]
+print('timestamp: ', ts)
+print('audio: ', audio)
+print('dist: ', dist)
+print('angle: ', angle)
--- create_raw_audio_dataset.py
+++ create_raw_audio_dataset.py
@@ -84,7 +84,7 @@ def create_interpolated_ground_truth(logs_id):
 gt_valid = df_gt.dropna()  # drop the data samples with no GT labels
 
 ## Create the h5 file and write to it
-dataset_home = '/workspace/data/raw_audio_and_groundtruth.h5'
+dataset_home = '/optional_dirs/datasets/raw_audio_and_groundtruth.h5'
 print('Creating h5 file at location ', dataset_home)
 f = h5py.File(dataset_home, 'w')
 
